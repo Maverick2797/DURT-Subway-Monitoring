b6520469a4bd2a056847d3ec8f9934eef6843919
Maverick2797 <49946595+Maverick2797@users.noreply.github.com>
2020-01-01 22:14:22 +0800

    Terminus Variant
    Added a Terminus variant of the normal light controller to account for unintuitiveness. Also remapped ports for current wall orientation

--- DURT-S23 Subway wall.lua
+++ DURT-S23 Subway wall.lua
@@ -1,9 +1,21 @@
 --set stn_code to the station's code, and ports as needed
 local stn_code = ""
-local nlight = "d"
-local slight = "b"
+local nlight = "c"
+local slight = "a"
 if event.type=="digiline" and event.channel=="status_update" then
 	if event.msg.N[stn_code] then port[nlight] = true else port[nlight] = false end
 	if event.msg.S[stn_code] then port[slight] = true else port[slight] = false end
+	return
+end
+------------------------------
+--terminus variant
+--set stn_code to the station's code, 
+	--direction to the new direction after exiting the terminus,
+	--and ports->lights as needed
+local stn_code = ""
+local direction = ""
+local light = "a"
+if event.type=="digiline" and event.channel=="status_update" then
+	if event.msg[direction][stn_code] then port[light] = true else port[light] = false end
 	return
 end
