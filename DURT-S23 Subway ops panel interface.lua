e99e5e8ddb94fe67496fe6f026315529cce61a10
Maverick2797 <49946595+Maverick2797@users.noreply.github.com>
2020-07-29 20:24:24 +0800

    unify interface
    allow the info being sent to be changed as a vairable, means less code to change between use cases

--- DURT-S23 Subway ops panel interface.lua
+++ DURT-S23 Subway ops panel interface.lua
@@ -1,4 +1,5 @@
-local line = "S23"
 if event.punch or (event.channel=="clock" and event.msg=="pulse")then
-digiline_send("status_update",S.lines[line].monitoring)
+local line = "S23"
+local info = S.lines[line].monitoring
+digiline_send("status_update",info)
 end
